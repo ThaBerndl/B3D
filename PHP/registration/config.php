3e5d9cc105e13e6665f1ba94150346672ea3455b
Stephan Berndl <berndlstephan@gmail.com>
2022-03-02 13:58:40 +0100

    .

--- PHP/registration/config.php
+++ PHP/registration/config.php
@@ -1,16 +1,29 @@
 <?php
-    /* Database credentials. Assuming you are running MySQL
-    server with default setting (user 'root' with no password) */
-    define('DB_SERVER', 'free-tier13.aws-eu-central-1.cockroachlabs.cloud');
-    define('DB_USERNAME', 'thaberndl');
-    define('DB_PASSWORD', '1wtodMZjBifk2xXrPRIRrw');
-    define('DB_NAME', 'defaultdb');
+    // /* Database credentials. Assuming you are running MySQL
+    // server with default setting (user 'root' with no password) */
+    // define('DB_SERVER', 'free-tier13.aws-eu-central-1.cockroachlabs.cloud');
+    // define('DB_USERNAME', 'thaberndl');
+    // define('DB_PASSWORD', '1wtodMZjBifk2xXrPRIRrw');
+    // define('DB_NAME', 'defaultdb');
     
-    /* Attempt to connect to MySQL database */
-    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    // /* Attempt to connect to MySQL database */
+    // $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
     
-    // Check connection
-    if($link === false){
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    }
+    // // Check connection
+    // if($link === false){
+    //     die("ERROR: Could not connect. " . mysqli_connect_error());
+    // }
+
+$servername = "free-tier13.aws-eu-central-1.cockroachlabs.cloud";
+$username = "thaberndl";
+$password = "1wtodMZjBifk2xXrPRIRrw";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
