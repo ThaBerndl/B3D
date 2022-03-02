030dd07af2d40e74a7c8fee6f445f60532541ebe
Stephan Berndl <berndlstephan@gmail.com>
2022-03-02 13:48:51 +0100

    testing DB

--- PHP/registration/config.php
+++ PHP/registration/config.php
@@ -1,10 +1,10 @@
 <?php
     /* Database credentials. Assuming you are running MySQL
     server with default setting (user 'root' with no password) */
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'B3D DB');
+    define('DB_SERVER', 'free-tier13.aws-eu-central-1.cockroachlabs.cloud');
+    define('DB_USERNAME', 'thaberndl');
+    define('DB_PASSWORD', '1wtodMZjBifk2xXrPRIRrw');
+    define('DB_NAME', 'defaultdb');
     
     /* Attempt to connect to MySQL database */
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
