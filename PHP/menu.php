d711cb6f6d55af53ea14c763d276478d650e5d38
Stephan Berndl <berndlstephan@gmail.com>
2022-03-18 10:48:35 +0100

    testing with user

--- PHP/menu.php
+++ PHP/menu.php
@@ -35,6 +35,8 @@
   // Create connection
   $conn = new mysqli($servername, $username, $password,$dbname,$port);
 
+  DB_addUser("Lena", "Wurmsdobler", "LenaPopena",null);
+
   // Check connection
   if ($conn->connect_errno) {
     printf("Connection failed: " . $conn->connect_error);
@@ -47,13 +49,16 @@
     printf("Error: %s/n", $mysqli->error);
   }
 
-  $tiere = $conn->query("SELECT bez from Tier");
+  $tiere = $conn->query("SELECT bez from User");
 
-  echo"Tiere: /n";
+  echo"User: ";
   for ($row_no = $tiere->num_rows-1; $row_no >= 0; $row_no--) { 
     $tiere->data_seek($row_no);
     $row = $tiere->fetch_assoc();
-    echo"bez = " . $row['bez'] . "/n";
+    echo"Vorname = " . $row['vName'] . "; \n";
+    echo"Nachname = " . $row['nName'] . "; \n";
+    echo"Nickname = " . $row['nickname'] . "; \n";
+    echo"Passwort = " . $row['passwort'] . "; \n";
   }
 ?>
 </html>
