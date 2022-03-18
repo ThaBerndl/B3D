4d1acb96e5eb2fd985042b2340a88350d7e3f73b
Stephan Berndl <berndlstephan@gmail.com>
2022-03-18 12:05:38 +0100

    added signup

--- PHP/registration/register.php
+++ PHP/registration/register.php
@@ -10,6 +10,29 @@
     
 </body>
 <?php
-  require conconfig.php;
+//  require conconfig.php;
+$servername = "localhost";
+$username = "root";
+$password = "raspberry";
+$dbname = "B3D";
+$port = 3306;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname,$port);
+
+// Check connection
+if ($conn->connect_errno) {
+  printf("Connection failed: " . $conn->connect_error);
+  exit();
+}
+
+
+$vorname = $_POST['vorname'];
+$nachname = $_POST['nachname'];
+$username = $_POST['username'];
+$pw = $_POST['password'];
+
+$conn->query("INSERT INTO User (vName,nName,nickname,passwort) values ('$vorname','$nachname','$username','$pw')");
+
 ?>
 </html>
