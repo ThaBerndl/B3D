b693d0fc594030c1a5e686ead93d2f4db5d6a368
Natotea <lukasnatotea@gmail.com>
2022-03-18 11:08:27 +0100

    registration html

--- PHP/registration/register.php
+++ PHP/registration/register.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration...</title>
+</head>
+<body>
 <?php
   $servername = "localhost";
   $username = "root";
@@ -25,3 +34,5 @@ if ($conn->query($sql) === TRUE) {
 
 $conn->close();
 ?>
+</body>
+</html>
