6bf43a8fda69e97dcf610ca88c7082ecde47d94a
Natotea <lukasnatotea@gmail.com>
2022-03-18 11:15:15 +0100

    removed html code

--- PHP/registration/register.php
+++ PHP/registration/register.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration...</title>
-</head>
-<body>
 <?php
   $servername = "localhost";
   $username = "root";
@@ -36,5 +27,3 @@ if ($conn->query($sql) === TRUE) {
 
 $conn->close();
 ?>
-</body>
-</html>
