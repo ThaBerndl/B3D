9dd8ac5181a988bc1c057aa7634b512bf1cfd9d5
Stephan Berndl <berndlstephan@gmail.com>
2022-03-22 22:02:42 +0100

    debugs

--- PHP/archer_conf.php
+++ PHP/archer_conf.php
@@ -1,4 +1,5 @@
 <?php
+    echo 'Sart Test';
 
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
@@ -7,6 +8,7 @@
     $port = 3306;
   
     $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
    
     // Check connection
     if($mysqli === false){
