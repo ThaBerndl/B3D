b0941beef1729b2605ffe4736d72c940c2b3eede
Stephan Berndl <berndlstephan@gmail.com>
2022-03-22 21:59:12 +0100

    debugs

--- PHP/archer_conf.php
+++ PHP/archer_conf.php
@@ -17,12 +17,15 @@
     $vname = $_POST['fname'];
     $nname = $_POST['lname'];
 
+    echo $nickname . ' ' . $vname . ' ' . $nname . '<br>';
+ 
     if($result = $mysqli->query("SELECT * FROM User WHERE nickname = '$nickname'"))
     {
         if($result->num_rows)
         {
             while($row = $result->fetch_assoc())
             {
+                echo $row['vName'] . ' ' . $row['nName'] . '<br>';
                 $vname = $row['vName'];
                 $nname = $row['nName'];
             }
@@ -31,13 +34,16 @@
         {
             if($mysqli->query("INSERT INTO User ('user_id', 'vName', 'nName', 'nickname', 'passwort') VALUES (NULL, '$vname', '$nname', '$nickname', NULL);"))
             {
+                echo 'insert <br>';
                 if($result = $mysqli->query("SELECT * FROM User WHERE nickname = '$nickname'"))
                 {
+                    echo 'select geglückt!';
                     if($result->num_rows)
                     {
                         while($row = $result->fetch_assoc())
                         {
                             $mysqli->query("INSERT INTO Freund ('user_id', 'freund_id') VALUES (1, '" . $row['user_id'] . "');"); //TODO: user_id von Login bekommen und hier hinein schreiben
+                            echo $row['user_id'] . '<br>';
                         }
                     }
                 }
