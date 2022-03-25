109c8c68197986ba1b0af8a45570bad7dbed8b8b
Louiiiiii <leon.oberndorfer@gmail.com>
2022-03-25 11:55:28 +0100

    kijbnopjn

--- PHP/registration/config.php
+++ PHP/registration/config.php
@@ -1,14 +1,19 @@
 <?php
-  define('DB_SERVER', 'localhost');
-  define('DB_USERNAME', 'root');
-  define('DB_PASSWORD','raspberry');
-  define('DB_NAME', 'B3D');
-  $port = 3306;
-
-  $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-  // Check connection
-  if($mysqli === false){
-      die("ERROR: Could not connect. " . $mysqli->connect_error);
+  class DB
+  {
+      private $host = 'localhost';
+      private $dbname = "B3D";
+      private $user = "root";
+      private $pwd = "raspberry";
+      public $pdo;
+      public function __construct()
+      {
+          try {
+              $this->pdo = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pwd);
+          }catch (Exception $e)
+          {
+              echo $e;
+          }
+      }
   }
 ?>
