647f8daf78305bb1cab4c6f448bffa5b2bd02b21
lenapopena <wurmsdobler.lena@gmail.com>
2022-03-26 23:47:11 +0100

    removed icon

--- pages/archer.php
+++ pages/archer.php
@@ -167,7 +167,6 @@
         </nav>
         <!-- End Navbar -->
         <div class="container-fluid py-4">
-<<<<<<< HEAD
             <!--TODO-->
             <form id="add_friend" action="../PHP/archer_conf.php" method="POST">
             <div class="row">
@@ -176,22 +175,7 @@
                         <div class="card-header pb-0">
                             <div class="d-flex align-items-center">
                                 <h6>Add a Friend</h6>
-                                    <input type="submit" class="btn btn-success btn-md ms-auto" value="Add Friend" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-                                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                                  </svg></input>
-=======
-            <form id="add_friend" action="archer.php" method="POST">
-                <div class="row">
-                    <div class="col-md-8">
-                        <div class="card">
-                            <div class="card-header pb-0">
-                                <div class="d-flex align-items-center">
-                                    <h6>Add a Friend</h6>
-                                        <input type="submit" name="submit" class="btn btn-success btn-md ms-auto" value="Add Friend" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-                                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                                    </svg></input>
-                                </div>
->>>>>>> 99a86ff3829e1444be26d797f6f844834709d490
+                                    <input type="submit" class="btn btn-success btn-md ms-auto" value="Add Friend"input>
                             </div>
                             <div class="card-body">
                                 <div class="row">
