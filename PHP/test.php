d65ccf3a7f42ea79e3336eb87537e928160b7532
Louiiiiii <leon.oberndorfer@gmail.com>
2022-03-27 01:45:52 +0100

    test12345

--- PHP/test.php
+++ PHP/test.php
@@ -1,6 +1,7 @@
 <?php
-echo test1234;
-require_once "class/User.php";
-$user = new User("Tim", "Hofmann", "thistim", "1234");
-echo $user->checkUser();
+    echo test1234;
+    require_once "class/User.php";
+    $user = new User("Tim", "Hofmann", "thistim", "1234");
+    $user->insertUser();
+    echo $user->checkUser();
 ?>
