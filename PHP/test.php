58751ad3fd455da9922f76a1d501253cb37d8686
Louiiiiii <leon.oberndorfer@gmail.com>
2022-03-27 01:55:52 +0100

    test.php -> 1

--- PHP/test.php
+++ PHP/test.php
@@ -1,7 +1,14 @@
 <?php
-    echo test1234;
     require_once "class/User.php";
     $user = new User("Tim", "Hofmann", "thistim", "1234");
     $user->insertUser();
-    echo $user->checkUser();
+
+    if($user->checkUser())
+    {
+        echo "an Tim gibts";
+    }
+    else
+    {
+        echo "an Tim gibts ned";
+    }
 ?>
