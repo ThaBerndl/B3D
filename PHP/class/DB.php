08077312573abee889aecd6a925cb284e19c6e02
Louiiiiii <leon.oberndorfer@gmail.com>
2022-03-27 03:02:16 +0200

    test.php -> 1 und nextID geht

--- PHP/class/DB.php
+++ PHP/class/DB.php
@@ -20,5 +20,13 @@
       {
           $this->pdo = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pwd);
       }
+
+      public function nextId($table)
+      {
+          $stmt = $this->pdo->prepare("SELECT AUTO_INCREMENT FROM information_schema.tables WHERE table_name = ? AND table_schema = DATABASE()");
+          $stmt->execute([$table]);
+
+          return $stmt->fetch()["AUTO_INCREMENT"];
+      }
   }
 ?>
