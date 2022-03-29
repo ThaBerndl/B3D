0d060f6531d856790488ff86936c75124fce4e9c
ThaBerndl <99948970+ThaBerndl@users.noreply.github.com>
2022-03-29 22:41:36 +0200

    Added php with all reqire classes

---
+++ PHP/getClasses.php
@@ -0,0 +1,7 @@
+<?php
+require_once "../PHP/class/DB.php";
+require_once "../PHP/class/User.php";
+require_once "../PHP/class/Freund.php";
+require_once "../PHP/class/Parcour.php";
+require_once "../PHP/class/Ort.php";
+require_once "../PHP/class/Tier.php";
