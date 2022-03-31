824436cb8eb6e0289e9172fd1269af4fda12b8c7
Natotea <lukasnatotea@gmail.com>
2022-03-31 11:23:43 +0200

    Added check for existing users to update the password - removed checkUsersExists Added Session variables to sign-up

--- pages/archer.php
+++ pages/archer.php
@@ -1,17 +1,10 @@
-<!--
-=========================================================
-* Argon Dashboard 2 - v2.0.1
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://www.creative-tim.com/license)
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
+<?php
+    session_start();
+    if (!$_SESSION['auth'])
+    {
+        header("location: sign-in.php");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -33,13 +26,6 @@
     <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
     <!-- CSS Files -->
     <link id="pagestyle" href="../assets/css/argon-dashboard.css?v=2.0.1" rel="stylesheet" />
-    <?php
-       session_start();
-       if (!$_SESSION['auth'])
-       {
-           header("location: sign-in.php");
-       }
-     ?>
 </head>
 
 <body class="g-sidenav-show   bg-gray-100">
