660c5cc86141e890806fe6189508b7509110232f
Natotea <lukasnatotea@gmail.com>
2022-04-01 08:42:11 +0200

    Added header.php for links of all types (css/img/...) Added body_end.php for js files

--- pages/tables.php
+++ pages/tables.php
@@ -316,15 +316,13 @@
     <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
     <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
     <script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-        var options = {
-            damping: '0.5'
+        var win = navigator.platform.indexOf('Win') > -1;
+        if (win && document.querySelector('#sidenav-scrollbar')) {
+            var options = {
+                damping: '0.5'
+            }
+            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
         }
-        Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-    </script>
-    <script>
         function reload()
         {
             var ort = document.getElementById('Orte');
