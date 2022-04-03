e413b55b4200f00a2d8fb01d141a49a49d595c4d
lenapopena <99950937+lenapopena@users.noreply.github.com>
2022-04-03 14:34:48 +0200

    completed UI

--- pages/new-game.php
+++ pages/new-game.php
@@ -1,20 +1,21 @@
 <?php
-    session_start();
-    if (!$_SESSION['auth'])
-    {
-        header("location: sign-in.php");
-    }
+session_start();
+if (!$_SESSION['auth']) {
+    header("location: sign-in.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
     <link rel="icon" type="image/png" href="../assets/img/favicon.png">
     <title>
         B3D - Skillboard | New Game
     </title>
+    <!--CSS B3D-->
+    <link id="pagestyle" href="../assets/css/b3d-style.css" rel="stylesheet"/>
     <?php
     session_start();
     include "../PHP/header.php";
@@ -29,11 +30,13 @@ require_once '../PHP/leftHor_Navbar.php'
 ?>
 <main class="main-content position-relative border-radius-lg ">
     <!-- Navbar -->
-    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur"
+         data-scroll="false">
         <div class="container-fluid py-1 px-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
+                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:">Pages</a>
+                    </li>
                     <li class="breadcrumb-item text-sm text-white active" aria-current="page">New Game</li>
                 </ol>
                 <h6 class="font-weight-bolder text-white mb-0">New Game</h6>
@@ -46,46 +49,104 @@ require_once '../PHP/leftHor_Navbar.php'
     <!-- End Navbar -->
     <div class="container-fluid py-4">
         <div class="row">
-            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-                <div class="card">
+            <div class="col-12">
+                <div class="card mb-4">
                     <div class="card-body p-3">
                         <h6>Start a new game</h6>
                         <div class="table-responsive">
                             <table class="table align-items-center justify-content-center mb-0">
                                 <tbody>
-                                <!--form-->
-                                <form id="choose_parcour" action="new-game.php" method="get">
-                                    <tr scope="row">
-                                        <td colspan="4">
-                                            <label for="example-text-input" class="form-control-label">Add archers</label>
-                                        </td>
-                                    </tr>
-                                    <tr scope="row">
-                                        <td colspan="4">
-                                            <label for="example-text-input" class="form-control-label">Add parcour</label>
-                                        </td>
-                                    </tr>
-                                    <tr scope="row">
-                                        <td colspan="4">
-                                            <label for="example-text-input" class="form-control-label">Chose scoring system</label>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td id=addParcourBtn>
-                                            <button type="submit" class="btn btn-success align-right">Enter</button>
-                                        </td>
-                                    </tr>
-                                </form>
+                                    <!--form-->
+                                    <form id="new_game_form" action="new-game.php" method="get">
+                                        <tr scope="row">
+                                            <!--Friendlist - select participating archers-->
+                                            <table class="table-responsive">
+                                                <table class="table align-items-center justify-content-center mb-0">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="row" class="archerNr">Friend</th>
+                                                            <th scope="row"></th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>
+                                                                <h6 class="mb-0 text-xs" name="archerName">Stephan
+                                                                    Berndl</h6>
+                                                                <p class="text-xs text-secondary mb-0"
+                                                                   name="archerNickname">@thaberndl</p>
+                                                            </td>
+                                                            <td>
+                                                                <div class="form-check centerCheckBox">
+                                                                    <input class="form-check-input" type="checkbox" value=""
+                                                                           name="checkArcher" checked="">
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                        <!--placeholder - might delete later-->
+                                                        <tr>
+                                                            <td>
+                                                                <h6 class="mb-0 text-xs" name="archerName">Lukas
+                                                                    Natotea</h6>
+                                                                <p class="text-xs text-secondary mb-0"
+                                                                   name="archerNickname">@lukiwuki</p>
+                                                            </td>
+                                                            <td>
+                                                                <div class="form-check centerCheckBox">
+                                                                    <input class="form-check-input" type="checkbox" value=""
+                                                                           name="checkArcher" checked="">
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                        <!--End placeholder-->
+                                                    </tbody>
+                                                </table>
+                                            </table>
+                                            <hr id="tables-hr">
+                                        </tr>
+                                        <!--End Friendslist-->
+                                        <tr scope="row">
+                                            <td colspan="4">
+                                                <label for="example-text-input" class="form-control-label">Parcour</label>
+                                                <select class="form-select" aria-label=".form-select-sm example">
+                                                    <option selected>-choose parcour-</option>
+                                                    <option value="1">One</option>
+                                                    <option value="2">Two</option>
+                                                    <option value="3">Three</option>
+                                                </select
+                                            </td>
+                                        </tr>
+                                        <hr id="tables-hr">
+                                        <tr scope="row">
+                                            <td colspan="4">
+                                                <label for="example-text-input" class="form-control-label">Scoring
+                                                    System</label>
+                                                <select class="form-select" aria-label=".form-select-sm example">
+                                                    <option selected>-choose scoring system-</option>
+                                                    <option value="Three arrow scoring">One</option>
+                                                    <option value="Two arrow scoring">Two</option>
+                                                </select>
+                                            </td>
+                                        </tr>
+                                        <hr id="tables-save-hr">
+                                        <tr>
+                                            <td>
+                                                <button type="submit" class="btn bg-gradient-success align-right" name="newGame">
+                                                    Start
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </form>
                                 </tbody>
                             </table>
                         </div>
                     </div>
                     <div class="card-body px-0 pt-0 pb-2">
                         <div class="table-responsive p-0"></div>
-                        </div>
                     </div>
                 </div>
             </div>
+        </div>
         <?php
         require_once "../PHP/footer.php";
         ?>
