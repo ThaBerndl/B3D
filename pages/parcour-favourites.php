b9a12e83b9bb428d8c9e7ab73ba3731fab5d47f7
lenapopena <99950937+lenapopena@users.noreply.github.com>
2022-04-03 17:16:33 +0200

    completed UI Design

--- pages/parcour-favourites.php
+++ pages/parcour-favourites.php
@@ -2,13 +2,18 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
     <link rel="icon" type="image/png" href="../assets/img/favicon.png">
     <title>
         B3D - Skillboard | Parcour Favourites
     </title>
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!--CSS B3D-->
+    <link id="pagestyle" href="../assets/css/b3d-style.css" rel="stylesheet"/>
     <?php
     session_start();
     include "../PHP/header.php";
@@ -23,11 +28,13 @@ require_once '../PHP/leftHor_Navbar.php'
 ?>
 <main class="main-content position-relative border-radius-lg ">
     <!-- Navbar -->
-    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur"
+         data-scroll="false">
         <div class="container-fluid py-1 px-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
+                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript;">Pages</a>
+                    </li>
                     <li class="breadcrumb-item text-sm text-white active" aria-current="page">Parcour Favourites</li>
                 </ol>
                 <h6 class="font-weight-bolder text-white mb-0">Parcour Favourites</h6>
@@ -40,19 +47,110 @@ require_once '../PHP/leftHor_Navbar.php'
     <!-- End Navbar -->
     <div class="container-fluid py-4">
         <div class="row">
-            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-                <div class="card">
+            <div class="col-12">
+                <div class="card mb-4">
                     <div class="card-body p-3">
                         <h6>Favourites</h6>
-                        <div class="table-responsive">
-                            <table class="table align-items-center justify-content-center mb-0">
-                                <tbody>
-                                <form id="choose_parcour" action="parcour-favourites.php" method="get">
-                                    <!--form-->
-                                </form>
-                                </tbody>
+                        <br>
+                        <!--form-->
+                        <form id="choose_parcour" action="parcour-favourites.php" method="get">
+                            <button type="submit"
+                                    class="btn btn-success align-right" name="add_parcour">Create Parcour&nbsp;&nbsp;<i
+                                        class="ni ni-fat-add"></i></button>
+                            <hr id="tables-hr">
+                            <button type="submit"
+                                    class="btn btn-outline-success align-right" name="saved_parcours">Saved parcours&nbsp;&nbsp;<i
+                                        class="ni ni-bold-down"></i></button>
+                            <table class="responsive">
+                                <table class="table align-items-center mb-0">
+                                    <thead>
+                                    <tr>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Parcour
+                                        </th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                            Animals
+                                        </th>
+                                        <th class="text-secondary opacity-7"></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <h6 class="mb-0 text-xs">Parcour 1</h6>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
+
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <h6 class="mb-0 text-xs">42</h6>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="align-middle">
+                                            <a href="javascript:;" class="text-secondary font-weight-bold text-xs"
+                                               data-toggle="tooltip" data-original-title="Delete parcour">
+                                                Delete
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
                             </table>
-                        </div>
+                            <hr id="tables-hr">
+                            <button type="submit"
+                                    class="btn btn-outline-success align-right" name="available_parcours">Available
+                                parcours&nbsp;&nbsp;<i class="ni ni-bold-down"></i></button>
+                            <table class="responsive">
+                                <table class="table align-items-center mb-0">
+                                    <thead>
+                                    <tr>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Parcour
+                                        </th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                            Animals
+                                        </th>
+                                        <th class="text-secondary opacity-7"></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <h6 class="mb-0 text-xs">Parcour 2</h6>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
+
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <h6 class="mb-0 text-xs">69</h6>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="align-middle">
+                                            <a href="javascript:;" class="text-secondary font-weight-bold text-xs"
+                                               data-toggle="tooltip" data-original-title="Add parcour">
+                                                Add
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </table>
+                            <hr id="tables-save-hr">
+                            <button id="saveParcourFavs" type="submit" name="saveParcourFavs"
+                                    class="btn bg-gradient-success">Save
+                            </button>
+                        </form>
+                        <!--End form-->
                     </div>
                     <div class="card-body px-0 pt-0 pb-2">
                         <div class="table-responsive p-0"></div>
