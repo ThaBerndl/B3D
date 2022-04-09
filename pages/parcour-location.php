f0d612af03313a3bb049390f24b0fa1a08f29a46
ThaBerndl <99948970+ThaBerndl@users.noreply.github.com>
2022-04-09 14:56:39 +0200

    testing

--- pages/parcour-location.php
+++ pages/parcour-location.php
@@ -216,7 +216,9 @@ require "../PHP/body_end.php";
                     $tierzuord->parcour_id = $parcour_ID;
                     $tierzuord->pos = ($i + 1);
                     $tierzuord->updateTier();
+                    echo '<script>alert("im If")</script>';
                 }
+                echo '<script>alert("am Ende")</script>';
             }
     ?>
 </script>
