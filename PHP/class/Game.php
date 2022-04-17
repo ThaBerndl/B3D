3c45edd7915293794bd400cd477fc113256820ed
ThaBerndl <99948970+ThaBerndl@users.noreply.github.com>
2022-04-17 23:25:17 +0200

    implemented logic behind results tab

--- PHP/class/Game.php
+++ PHP/class/Game.php
@@ -21,4 +21,26 @@ class Game extends DB{
         $data = $stmt->fetchAll();
         return new Game($data[0]['game_id'],$data[0]['created'],$data[0]['parcour_id']);
     }
+
+    public static function getGamesFromUser($user_id){
+        $db = new DB();
+        $stmt = $db->pdo->prepare("select g.game_id 'game_id',
+                                                   p.bez 'parcour', 
+                                                   o.bez 'ort', 
+                                                   g.created 'created' 
+                                              from parcour p,
+                                                   ort o,
+                                                   game g,
+                                                   punktestand pu
+                                             where p.ort_id = o.ort_id
+                                               and g.parcour_id = p.parcour_id
+                                               and pu.game_id = g.game_id
+                                               and user_id = ?
+                                             group by p.bez, o.bez, g.created
+                                             order by created desc");
+        $stmt->bindParam(1,$user_id, PDO::PARAM_INT);
+        $stmt->execute();
+        $error=$stmt->errorInfo();
+        return $stmt;
+    }
 }
