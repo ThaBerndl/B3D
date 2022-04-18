ae08aa7ca83d8adccee0d2af0faf17a8f0b447f3
lenapopena <99950937+lenapopena@users.noreply.github.com>
2022-04-18 10:28:31 +0200

    added tables

--- pages/scoreboard.php
+++ pages/scoreboard.php
@@ -1,17 +1,19 @@
-/*info:
+<!--info:
   this page won't be linked in leftHor_Navbar.php and will only be accessible through result.php
-*/
+  -->
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
     <link rel="icon" type="image/png" href="../assets/img/favicon.png">
     <title>
         B3D - Skillboard | Scoreboard
     </title>
+    <!--CSS B3D-->
+    <link id="pagestyle" href="../assets/css/b3d-style.css" rel="stylesheet"/>
     <?php
     session_start();
     include "../PHP/header.php";
@@ -41,28 +43,152 @@ require_once '../PHP/leftHor_Navbar.php'
         </div>
     </nav>
     <!-- End Navbar -->
+    <!--Game Details-->
     <div class="container-fluid py-4">
-        <div class="row">
-            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+        <div class="row mt-4">
+            <div class="col-12">
                 <div class="card">
                     <div class="card-body p-3">
-                        <h6>Scoreboard</h6>
-                        <div class="table-responsive">
-                            <table class="table align-items-center justify-content-center mb-0">
-                                <tbody>
-                                <form id="choose_parcour" action="scoreboard.php" method="get">
-                                    <!--form-->
-                                </form>
-                                </tbody>
-                            </table>
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="numbers">
+                                    <p class="text-sm mb-0 text-uppercase font-weight-bold" name="gameDate"><i
+                                                class="ni ni-calendar-grid-58"></i>&nbsp;17.04.2022</p>
+                                    <h5 class="font-weight-bolder" name="parcourLoc">
+                                        BSV Maria Taferl
+                                    </h5>
+                                    <p class="mb-0 text-sm">
+                                        <span class="text-success text-sm font-weight-bolder" name="gameId">#</span>
+                                        123456789
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-4 text-end">
+                                <div class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
+                                    <i class="ni ni-pin-3 text-lg opacity-10" aria-hidden="true"></i>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="card-body px-0 pt-0 pb-2">
-                        <div class="table-responsive p-0"></div>
                     </div>
                 </div>
             </div>
         </div>
+        <!--End game details-->
+        <!--Total Score-->
+        <div class="row mt-4">
+            <div class="col-12">
+                <div class="card ">
+                    <div class="card-header pb-0 p-3">
+                        <div class="d-flex justify-content-between">
+                            <h6 class="mb-2">Total Score</h6>
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table align-items-center">
+                            <tr>
+                                <th class="text-uppercase text-xxs font-weight-bolder mb-0" scope="row">Archer</th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+                                    scope="row">Stephan
+                                </th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+                                    scope="row">Lukas
+                                </th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+                                    scope="row">Leon
+                                </th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+                                    scope="row">Lena
+                                </th>
+                            </tr>
+                            <tr>
+                                <th class="text-uppercase text-xxs font-weight-bolder mb-0" scope="row">Total</th>
+                                <td class="text-xs font-weight-bold mb-0">408</td>
+                                <td class="text-xs font-weight-bold mb-0">246</td>
+                                <td class="text-xs font-weight-bold mb-0">210</td>
+                                <td class="text-xs font-weight-bold mb-0">132</td>
+                            </tr>
+                            <tr>
+                                <th class="text-uppercase text-xxs font-weight-bolder mb-0" scope="row">Hits</th>
+                                <td class="text-xs font-weight-bold mb-0">29/33</td>
+                                <td class="text-xs font-weight-bold mb-0">22/33</td>
+                                <td class="text-xs font-weight-bold mb-0">18/33</td>
+                                <td class="text-xs font-weight-bold mb-0">13/33</td>
+                            </tr>
+                            <tr>
+                                <th class="text-uppercase text-xxs font-weight-bolder mb-0" scope="row">Arrow 1</th>
+                                <td class="text-xs font-weight-bold mb-0">18</td>
+                                <td class="text-xs font-weight-bold mb-0">11</td>
+                                <td class="text-xs font-weight-bold mb-0">6</td>
+                                <td class="text-xs font-weight-bold mb-0">5</td>
+                            </tr>
+                            <tr>
+                                <th class="text-uppercase text-xxs font-weight-bolder mb-0" scope="row">Arrow 2</th>
+                                <td class="text-xs font-weight-bold mb-0">9</td>
+                                <td class="text-xs font-weight-bold mb-0">3</td>
+                                <td class="text-xs font-weight-bold mb-0">8</td>
+                                <td class="text-xs font-weight-bold mb-0">2</td>
+                            </tr>
+                            <tr>
+                                <th class="text-uppercase text-xxs font-weight-bolder mb-0" scope="row">Arrow 3</th>
+                                <td class="text-xs font-weight-bold mb-0">2</td>
+                                <td class="text-xs font-weight-bold mb-0">8</td>
+                                <td class="text-xs font-weight-bold mb-0">4</td>
+                                <td class="text-xs font-weight-bold mb-0">6</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--End Total Score-->
+        <!--Target Overview-->
+        <div class="row mt-4">
+            <div class="col-12">
+                <div class="card ">
+                    <div class="card-header pb-0 p-3">
+                        <div class="d-flex justify-content-between">
+                            <h6 class="mb-2">Targets</h6>
+                        </div>
+                    </div>
+                    <div class="table-responsive tableFixHead">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th class="text-uppercase text-xxs font-weight-bolder mb-0" scope="row">Target</th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder" scope="row">
+                                    Stephan
+                                </th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder" scope="row">
+                                    Lukas
+                                </th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder" scope="row">Leon
+                                </th>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder" scope="row">Lena
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td class="text-uppercase text-xxs font-weight-bolder mb-0" scope="row">#1</td>
+                                <td class="text-xs font-weight-bold mb-0">10</td>
+                                <td class="text-xs font-weight-bold mb-0">4</td>
+                                <td class="text-xs font-weight-bold mb-0">4</td>
+                                <td class="text-xs font-weight-bold mb-0">0</td>
+                            </tr>
+                            <tr>
+                                <td class="text-uppercase text-xxs font-weight-bolder mb-0" scope="row">#2</td>
+                                <td class="text-xs font-weight-bold mb-0">16</td>
+                                <td class="text-xs font-weight-bold mb-0">16</td>
+                                <td class="text-xs font-weight-bold mb-0">0</td>
+                                <td class="text-xs font-weight-bold mb-0">16</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Target Overview-->
         <?php
         require_once "../PHP/footer.php";
         ?>
@@ -164,6 +290,7 @@ require "../PHP/body_end.php";
         Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
     }
 </script>
+<script src="/assets/js/plugins/chartjs.min.js"></script>
 </body>
 
 </html>
