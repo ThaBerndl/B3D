4841e23ec87065fbc077991c83af98140d094390
lenapopena <99950937+lenapopena@users.noreply.github.com>
2022-04-18 18:34:32 +0200

    deleted irrelevant files

--- index.php
+++ index.php
@@ -15,7 +15,6 @@ else
 <head>
   <meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="assets/img/branding/circle logo_new.png">
   <title>
     B3D - Skillboard
